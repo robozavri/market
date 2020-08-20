94bbe61bcb76d03bd6f7ff85ddb9eba3f9dd98cb
robozavri <nika37912@mail.ru>
2020-08-20 10:29:07 +0400

    [feat][admin-category]: add category admin module

--- admin/src/app/routes/admin/routes/category/shared/form/basic-info/basic-info.component.ts
+++ admin/src/app/routes/admin/routes/category/shared/form/basic-info/basic-info.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl } from '@angular/forms';
 import { FormComponent } from 'app/shared/components/form.component';
 import { Category } from 'app/shared/models/category';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
+export interface State {
+  flag: string;
+  name: string;
+  population: string;
+}
 
 @Component({
   selector: 'app-basic-info',
@@ -11,18 +19,56 @@ import { Category } from 'app/shared/models/category';
 
 export class BasicInfoComponent extends FormComponent implements OnInit {
 
+  
+  stateCtrl = new FormControl();
+  filteredStates: Observable<State[]>;
 
+  states: State[] = [
+    {
+      name: 'Arkansas',
+      population: '2.978M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Arkansas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/9/9d/Flag_of_Arkansas.svg'
+    },
+    {
+      name: 'California',
+      population: '39.14M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_California.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Flag_of_California.svg'
+    },
+    {
+      name: 'Florida',
+      population: '20.27M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Florida.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Florida.svg'
+    },
+    {
+      name: 'Texas',
+      population: '27.47M',
+      // https://commons.wikimedia.org/wiki/File:Flag_of_Texas.svg
+      flag: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Texas.svg'
+    }
+  ];
   constructor(
     private fb: FormBuilder,
   ) {
     super();
+    this.filteredStates = this.stateCtrl.valueChanges
+      .pipe(
+        startWith(''),
+        map(state => state ? this._filterStates(state) : this.states.slice())
+      );
   }
+
+  private _filterStates(value: string): State[] {
+    const filterValue = value.toLowerCase();
+    return this.states.filter(state => state.name.toLowerCase().indexOf(filterValue) === 0);
+  }
+
   @Input() formData: any;
   @Input() amenities: any;
   @Input() showSubmit = true;
   @Output() submitForm = new EventEmitter<Category>();
-
-  serviceType: string;
 
   form: FormGroup;
 
@@ -30,6 +76,7 @@ export class BasicInfoComponent extends FormComponent implements OnInit {
     this.loadForm();
   }
 
+  
   loadForm(): void {
 
     this.formData.title = this.formData.title || {};
