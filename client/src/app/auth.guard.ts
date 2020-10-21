de262e5a02be4ecd1dbda388de1def8d8f38fad5
robozavri <nika37912@hotmail.com>
2020-10-21 20:38:21 +0400

    [feat][client-app]: update auth guard

--- client/src/app/auth.guard.ts
+++ client/src/app/auth.guard.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { UserApiService } from './shared/http/user-api.service';
 import { AuthService } from './shared/services/auth.service';
+import { SocketService } from './shared/services/socket.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +13,34 @@ export class AuthGuard implements CanActivate {
 
   constructor(
     private authService: AuthService,
+    private userApiService: UserApiService,
+    private socketService: SocketService,
+    private router: Router
     ) {}
+
+  // canActivate(
+    // next: ActivatedRouteSnapshot,
+    // state: RouterStateSnapshot
+  //   ): boolean {
+  //     console.log('this.authService.isSigned()',this.authService.isSigned());
+  //     if (this.authService.isSigned()) {
+  //       return true;
+  //     }
+  //     return false;
+  // }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-    ): boolean {
-      console.log('this.authService.isSigned()',this.authService.isSigned());
+    ): Observable<boolean> {
+    return this.userApiService.getMe().pipe(map((user) => {
+      this.authService.changeUser(user); console.log('AuthGuard user',user)
       if (this.authService.isSigned()) {
+        // this.socketService.init();
         return true;
       }
+      this.router.navigate(['/home']);
       return false;
+    }));
   }
 }
