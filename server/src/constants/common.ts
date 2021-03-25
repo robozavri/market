4229796a350731aab0b7e5d1df308f14e1368817
robozavri <nika37912@hotmail.com>
2021-03-25 19:32:14 +0400

    [feat][server-package.js constatns]: add image sizes and install packages

--- server/src/constants/common.ts
+++ server/src/constants/common.ts
@@ -4,6 +4,11 @@ export const contacts = {
   MAIL_TO: '12giorgi@gmail.com',
 };
 
+export const imageConfig = {
+  desiredPrintWidth: 500,
+  desiredPrintHeight: 800,
+};
+
 export const langs = {
   EN: 'en',
   GE: 'ge',
